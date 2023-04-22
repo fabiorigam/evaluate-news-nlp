2e2fd96cd1b4776bc891e20910925c6e59167e38
fabiorigam <fabio.rigamonti@icloud.com>
2023-04-22 13:51:19 +0200

    add checkPopularity and workbox plugin

--- __test__/testPolarityChecker.spec.js
+++ __test__/testPolarityChecker.spec.js
@@ -1,26 +1,26 @@
 import "babel-polyfill";
-import { polarityChecker } from "../src/client/js/formHandler"
+import { checkPopularity } from "../src/client/js/formHandler"
 
 describe('Testing polarity translation functionality', () => {
     test('Testing the polarityChecker() function', () => {
-       expect(polarityChecker('P+')).toBe('STRONG POSITIVE')
+       expect(checkPopularity('P+')).toBe('STRONG POSITIVE')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker('P')).toBe('POSITIVE')
+        expect(checkPopularity('P')).toBe('POSITIVE')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker('NEW')).toBe('NEUTRAL')
+        expect(checkPopularity('NEW')).toBe('NEUTRAL')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker('N')).toBe('NEGATIVE')
+        expect(checkPopularity('N')).toBe('NEGATIVE')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker('N+')).toBe('STRONG NEGATIVE')
+        expect(checkPopularity('N+')).toBe('STRONG NEGATIVE')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker('NONE')).toBe('NO SENTIMENT')
+        expect(checkPopularity('NONE')).toBe('NO SENTIMENT')
     })
     test('Testing the polarityChecker() function', () => {
-        expect(polarityChecker).toBeDefined();
+        expect(checkPopularity).toBeDefined();
     })
 });
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -9,8 +9,33 @@ function handleSubmit(event) {
     fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = "Polarity: "+checkPopularity(res.score_tag);
     })
 }
 
+const checkPopularity = (input) => {
+    let result;
+    switch (input){
+        case 'P+':
+            result = 'strong positive';
+            break;
+        case 'P':
+            result = 'positive';
+            break;
+        case 'NEW':
+            result = 'neutral';
+            break;
+        case 'N':
+            result = 'negative';
+            break;
+        case 'N+':
+            result = 'strong negative';
+            break;
+        case 'NONE':
+            result = 'no sentiment';
+    }
+    return result.toUpperCase();
+}
+
 export { handleSubmit }
+export {checkPopularity}
